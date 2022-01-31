8f92e3e63df57231d22c7e926ac4a633fbf83700
CvetiTo <email@example.com>
2022-01-31 19:28:24 +0200

    Impemented car listing , and added frond-end code

---
+++ static/js/app.js
@@ -0,0 +1,17 @@
+//alert('it works');
+document.getElementById('cars').addEventListener('click', (ev) => {
+    
+    if(ev.target.classList.contains('more')) {
+        //console.log(ev.target);
+        const btn = ev.target;
+        const desc = ev.target.parentElement.querySelector('.description');
+        if(desc.style.display == 'block') {
+            desc.style.display = 'none';
+            ev.target.textContent =
+             'Show More';
+        } else {
+            desc.style.display = 'block';
+            ev.target.textContent = 'Hide It'
+        }
+    }
+});
